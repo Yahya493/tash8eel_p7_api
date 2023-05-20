fc6231c13a882145eafe2bd4c31e1587b8c54e95
Yahya493 <78732281+Yahya493@users.noreply.github.com>
2023-05-21 01:58:04 +0300

    add databaseURL

--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,35 @@
 const express = require('express')
 const router = express.Router()
+module.exports = router
 
+const User = require('../models/user')
+const insertUser = async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        password: req.body.password,
+        birthDate: req.body.date,
+        address: req.body.address,
+        phone: req.body.phone,
+        email: req.body.email,
+        image: req.body.image,
+        type: req.body.type,
+        description: req.body.description
+    })
 
-router.post('/', async (req, res) => {
-    
-})
+    try {
+        const dataToSave = await user.save()
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
 
-router.get('/data', async (req, res) => {
-    
-})
+router.post('/insertUser', insertUser)
 
-router.patch('/update/:id', async (req, res) => {
-    
-})
+router.get('/data', async (req, res) => {})
 
-router.delete('/delete/:id', async (req, res) => {
-    
-})
+router.patch('/update/:id', async (req, res) => {})
+
+router.delete('/delete/:id', async (req, res) => {})
+
