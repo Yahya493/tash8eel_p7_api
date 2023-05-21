7eea4895eecede064a78168729cb64155d31e259
Yahya493 <78732281+Yahya493@users.noreply.github.com>
2023-05-21 14:17:20 +0300

    add userHelper

---
+++ helpers/userHelper.js
@@ -0,0 +1,68 @@
+const User = require('../models/user')
+
+const insertUser = async (req, res) => {
+    // const user = new User({
+    //     name: req.body.name,
+    //     password: req.body.password,
+    //     birthDate: req.body.birthDate,
+    //     address: req.body.address,
+    //     phone: req.body.phone,
+    //     email: req.body.email,
+    //     image: req.body.image,
+    //     type: req.body.type,
+    //     description: req.body.description
+    // })
+
+    const user = new User(req.body)
+
+    try {
+        const dataToSave = await user.save()
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+const getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.send(user)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndRemove(req.params.id)
+        const status = user?'deleted':'not found'
+        res.send({status: status, data:user})
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+const updateUser = async (req, res) => {
+    try {
+        const id = req.params.id
+        const updatedUser = req.body
+        const options = {new: true}
+
+        const data = await User.findByIdAndUpdate(id, updatedUser, options)
+        res.send(data)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+module.exports = {
+    insertUser,
+    getUserById,
+    deleteUser,
+    updateUser,
+
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,35 +1,9 @@
 const express = require('express')
+const { insertUser, deleteUser, getUserById, updateUser } = require('../helpers/userHelper')
 const router = express.Router()
 module.exports = router
 
-const User = require('../models/user')
-const insertUser = async (req, res) => {
-    const user = new User({
-        name: req.body.name,
-        password: req.body.password,
-        birthDate: req.body.date,
-        address: req.body.address,
-        phone: req.body.phone,
-        email: req.body.email,
-        image: req.body.image,
-        type: req.body.type,
-        description: req.body.description
-    })
-
-    try {
-        const dataToSave = await user.save()
-        res.status(200).json(dataToSave)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-}
-
 router.post('/insertUser', insertUser)
-
-router.get('/data', async (req, res) => {})
-
-router.patch('/update/:id', async (req, res) => {})
-
-router.delete('/delete/:id', async (req, res) => {})
-
+router.delete('/deleteUser/:id', deleteUser)
+router.get('/users/:id', getUserById)
+router.patch('/updateUser/:id', updateUser)
