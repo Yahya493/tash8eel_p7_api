628bb6f3df8b7bd32b00bbcd21f2b5b8f61effc1
Yahya493 <78732281+Yahya493@users.noreply.github.com>
2023-05-31 20:25:50 +0300

    .

--- helpers/userHelper.js
+++ helpers/userHelper.js
@@ -27,7 +27,7 @@ const insertUser = async (req, res) => {
 const getUserById = async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
-        res.send(user)
+        res.send(user==null?{}:user)
     }
     catch (error) {
         res.status(400).json({ message: error.message })
